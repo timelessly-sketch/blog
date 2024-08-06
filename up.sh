a6f6192116176fec9734d6d2db98456ff0bcd644
timelessly-sketch <2025907338@qq.com>
2024-08-06 15:50:21 +0800

    111

--- up.sh
+++ up.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-hexo clean && hexo deploy && hexo clean
+hexo clean && hexo deploy && 
 
 rsync -avzP -e "ssh -p20631" /Users/ylin/Documents/meng/public/ root@121.54.189.30:/www/wwwroot/link.71ll.cc
+
+hexo clean
+
 echo "上传成功！"
